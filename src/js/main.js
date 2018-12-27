d892e2ae2697281819898db624652d1a49ed8324
lnguyen24794 <lnguyen24794@gmail.com>
2018-12-28 02:26:58 +0700

    lamnt tooltip 75%

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,20 @@ function tooltip(){
         // console.log($arBtn[i]);
         if ($arBtn[i].hasAttribute("data-toggle")) {
            if($arBtn[i].getAttribute("data-toggle")=='tooltip'){
-            $arBtn[i].addEventListener("click",function(){
-                template = '<div class="tooltip" role="tooltip">' +
-                           '<div class="arrow"></div>' +
-                           '<div class="tooltip-inner"></div></div>';
-            });
+               position= $arBtn[i].getAttribute("data-placement");
+                template = '<div class="tooltip rbox_tooltip_'+position+'" role="tooltip">' +
+                '<div class="arrow"></div>' +
+                '<div class="tooltip_inner">'+$arBtn[i].innerHTML+'</div></div>';
+                $arBtn[i].innerHTML+=template;
+                $arBtn[i].setAttribute("title","");
+                $arBtn[i].addEventListener("mouseover",function(){
+                   
+                    this.children[0].classList.add("show");
+                    // this.children[0].style.top="10px";
+                });
+                $arBtn[i].addEventListener("mouseout",function(){
+                    this.children[0].classList.remove("show");
+                });
            }
         }
     }
