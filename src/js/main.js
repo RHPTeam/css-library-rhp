99c6a10003435d8b74eb9ca6578189b7e07f5ed5
ChinhHo <tuyetchinh1996@mail.com>
2019-01-02 16:45:04 +0700

    otherlayout Grid and Spacing

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-/*
-Notice: The code below just test, You can delete it and code normally. Enjoy!!!!
-*/
-
-console.log("Hello code one line");
-console.log("Hello code two line");
-console.log("Hello code three line");
-
